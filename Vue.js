d1db2d0dadb5673df345c311f05ab8b6960ed385
Nishida-eng <124713578+Nishida-eng@users.noreply.github.com>
2023-02-15 14:17:58 +0900

    Add files via upload

--- Vue.js
+++ Vue.js
@@ -3,21 +3,18 @@ new Vue({
   data () {
     return {
       message: '営業さんList',
-      books: null,
+      books: null
     }
   },
   mounted () {
-    fetch(
-      'https://script.google.com/macros/s/AKfycbxP5p9lHBQym_GAxp-vqfwLzjFkBc12kQOIJHuH61RWRVo35Xq1xxgzN4l3uOzsTS3V/exec'
-    )
+    fetch('https://script.google.com/macros/s/AKfycbxP5p9lHBQym_GAxp-vqfwLzjFkBc12kQOIJHuH61RWRVo35Xq1xxgzN4l3uOzsTS3V/exec')
       .then(res => res.json())
       .then(
       result => {
         this.books = result
       },
       error => {
-
-      },
+      }
     );
   }
 })
